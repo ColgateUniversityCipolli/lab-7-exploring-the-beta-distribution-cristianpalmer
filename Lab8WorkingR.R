4fe69f627dd72e91d2c6ca4153c05188e4d04166
cristianpalmer <cpalmer@colgate.edu>
2025-03-27 14:16:55 -0400

    onto task 8

--- Lab8WorkingR.R
+++ Lab8WorkingR.R
@@ -35,9 +35,12 @@ MOM.beta<- function(data, par){
   return(c(EX-m1, EX2-m2)) 
 }
 
-nleqslv(x = c(1,1), 
+(MOMs <- nleqslv(x = c(1,1), 
         fn = MOM.beta,
-        data= world_bank_data_2022$'Percent_Died')
+        data= world_bank_data_2022$'Percent_Died'))
+
+alpha.hat.mom <- MOMs$x[1]
+beta.hat.mom <- MOMs$x[2]
 
 ###################
 # MLE (Maximum Likelihood Estimator)
@@ -62,18 +65,64 @@ beta.hat.mle <- mles$par[2]
 ###################
 # Plot Histogram of Data Superimposed MOM and MLE Distributions
 ###################
-
+#Graph histogram of Data
 ggplot() +
   geom_histogram(data= world_bank_data_2022,
                  aes(x= Percent_Died,
                      y= after_stat(density)),
                  color="lightgrey") +
-  xlab("Percent of Population Which Died in 2022 For Various Countries")
+  xlab("Proportion of Population Which Died in 2022 For Various Countries")
 
-ggdat.exp <- tibble(x=seq(0,0.030,length.out=1000)) |>
-  mutate(pdf = dexp(x, rate=1/mean(world_bank_data_2022$'Percent_Died')))
-ggplot()+
-  geom_histogram(data=world_bank_data_2022,
-                 breaks = seq(0, 125, 25),
-                 color="lightgrey")+
-  geom_line(data=ggdat.exp)
+
+#Graph Estimated Max Likelihood
+Estimated.Max.Likelihood.Data <- tibble(x = seq(-0.0005, 0.025, length.out=1000))|>   # generate a grid of points
+  mutate(beta.pdf = dbeta(x, alpha.hat.mle, beta.hat.mle)) # Compute Beta PDF
+
+ggplot(data= Estimated.Max.Likelihood.Data)+                           # specify data
+  geom_line(aes(x=x, y=beta.pdf, color="Max L Distribution")) +                      # plot beta dist
+  geom_hline(yintercept=0)+                                            # plot x axis
+  theme_bw()+                                                          # change theme
+  xlab("x")+                                                           # label x axis
+  ylab("Density")+                                                     # label y axis
+  scale_color_manual("", values = c("blue"))+                          # change colors
+  theme(legend.position = "bottom") +                                  # move legend to bottom
+  ggtitle("Estimated Max Likelihood Distribution")                     # title graph  
+
+
+#Graph Estimated MOM
+Estimated.MOM.Data <- tibble(x = seq(-0.0005, 0.025, length.out=1000))|>   # generate a grid of points
+  mutate(beta.pdf = dbeta(x, alpha.hat.mom, beta.hat.mom)) # Compute Beta PDF
+
+ggplot(data= Estimated.MOM.Data)+                                      # specify data
+  geom_line(aes(x=x, y=beta.pdf, color="MOM Distribution")) +                       # plot beta dist
+  geom_hline(yintercept=0)+                                            # plot x axis
+  theme_bw()+                                                          # change theme
+  xlab("x")+                                                           # label x axis
+  ylab("Density")+                                                     # label y axis
+  scale_color_manual("", values = c("red"))+                           # change colors
+  theme(legend.position = "bottom") +                                  # move legend to bottom
+  ggtitle("Estimated Method of Moments Distribution")                     # title graph  
+
+
+
+#Superimpose
+ggplot() +
+  geom_histogram(data= world_bank_data_2022,
+                 aes(x= Percent_Died,
+                     y= after_stat(density)),
+                 color="lightgrey") +
+  xlab("Proportion of Population Which Died in 2022 For Various Countries") +
+  
+  geom_line(aes(x=x, y=beta.pdf, color="MOM Distribution"),
+            data = Estimated.MOM.Data) +                 
+  geom_hline(yintercept=0)+                                        
+  theme_bw() +
+  
+  geom_line(aes(x=x, y=beta.pdf, color="Max L Distribution"),
+            data = Estimated.Max.Likelihood.Data) +         
+  geom_hline(yintercept=0)+                                            
+  theme_bw() +
+  labs(color = "Distribution")
+
+#Task 8: Which estimators should we use?
+  
